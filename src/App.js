608c73314d25b3a1bd4dbb1b2afaf4348e5bb7d8
Bhavya Kapoor <bhavyakapoor1729@gmail.com>
2022-01-07 21:07:03 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-  <div>
-    <h1>Hats page Buddy</h1>
-  </div>
-  )
-  };
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="/hats" element={<HatsPage></HatsPage>}></Route>
         <Route path="/shop" element={<ShopPage></ShopPage>}></Route>
       </Routes>
     </div>
